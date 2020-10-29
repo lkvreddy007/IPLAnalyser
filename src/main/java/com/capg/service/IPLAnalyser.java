5c7253d3c2aed259807a39419a53f8303a2cd8fb
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-29 15:28:14 +0530

    [Krishna]refactored-IPLAnalyserClassToQualifyDryPrinciple

--- src/main/java/com/capg/service/IPLAnalyser.java
+++ src/main/java/com/capg/service/IPLAnalyser.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.capg.dto.IPLAnalyserException;
 import com.capg.dto.Batsman;
+import com.capg.dto.Bowler;
 import com.capg.dto.CsvLoader;
 
 public class IPLAnalyser<E> {
@@ -17,11 +18,11 @@ public class IPLAnalyser<E> {
 		return new CsvLoader<E>().csvFileLoader(csvFilePath, csvClass);
 	}
 	
-	public List<Batsman> sortBatsmenData(Comparator<Batsman> comparator, List<Batsman> csvList){
+	public List<E> sort(Comparator<E> comparator, List<E> csvList){
 		for(int j=0;j<csvList.size();j++) {
 			for(int i=0;i<csvList.size()-1;i++) {
-				Batsman player1 = csvList.get(i);
-				Batsman player2 = csvList.get(i+1);
+				E player1 = csvList.get(i);
+				E player2 = csvList.get(i+1);
 				if(comparator.compare(player1, player2)>0) {
 					csvList.set(i, player2);
 					csvList.set(i+1, player1);
