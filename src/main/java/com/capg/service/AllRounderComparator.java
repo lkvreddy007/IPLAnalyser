27ef9abf07fb69d841e62d11cae678ce1a25c7e5
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-29 22:41:00 +0530

    [Krishna]added-abilityToSortAllRoundersByRunsAndWickets

--- src/main/java/com/capg/service/AllRounderComparator.java
+++ src/main/java/com/capg/service/AllRounderComparator.java
@@ -7,7 +7,11 @@ import com.capg.dto.AllRounder;
 public class AllRounderComparator {
 
 	public Comparator<AllRounder> sortByCricketersBattingAnBowlingAverages() {
-		return Comparator.comparing(allrounder->allrounder.battingAverage*allrounder.bowlingAverage);
+		return Comparator.comparing(allrounder -> allrounder.battingAverage * allrounder.bowlingAverage);
+	}
+
+	public Comparator<AllRounder> sortByAllRounderRunsAndWickets() {
+		return Comparator.comparing(allrounder -> allrounder.battingRuns * allrounder.wickets);
 	}
 
 }
